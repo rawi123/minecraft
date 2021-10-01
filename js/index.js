5f82045c0ce55ef8c8481be1e0d60e0f173a5fc1
rawi123 <rawipro.rl@gmail.com>
2021-10-02 01:00:46 +0300

    before sleep

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function createElements() {
     small = document.querySelector("#small");
     big = document.querySelector("#big");
     board = document.querySelector(".board")
-    inventory=document.querySelector(".inventory")
+    inventory = document.querySelector(".inventory")
 }
 function assignEvenListeners() {//assign event for every button on the home screen
     startGame.addEventListener("click", e => {//open the game file 
@@ -38,34 +38,59 @@ function importBoard() {//does all the job to start the game
     board.style.width = boardSize + "vw"
     board.style.gridTemplateColumns = `repeat(${gameSize[0]},1fr)`;
     board.style.gridTemplateRows = `repeat(${gameSize[1]},1fr)`;
-    let classForInv=boardSize===100?"inventory-big":"inventory-small"
-    inventory.classList.add(`${classForInv}`,"inventory")
+    let classForInv = boardSize === 100 ? "inventory-big" : "inventory-small"
+    inventory.classList.add(`${classForInv}`, "inventory")
     createBoard()
     createInventory()
 }
-function createInventory(){//create inventory
-    let temp=["shovel","axe","pickaxe","sword"]
-    for (let i=0;i<8;i++){
-        let div=document.createElement("div")
-        inventoryArr.push(div)
+function createInventory() {//create inventory
+    let temp = ["shovel", "axe", "pickaxe", "sword"]
+    for (let i = 0; i < invSpace; i++) {
+        let div = document.createElement("div")
         div.classList.add("inv-item")
-        if(temp[i]){
-            let item=document.createElement("div")
-            item.classList.add(`${temp[i]}`,"taken-inv-item")
+        if (temp[i]) {
+            inventoryArr.push(div)
+            let item = document.createElement("div")
+            item.classList.add(`${temp[i]}`, "taken-inv-item")
             div.appendChild(item)
         }
-        else{emptyInventory.set(div,0)}
-        choosen=inventoryArr[0]
-        div.addEventListener("click",choose)
+        else { emptyInventory.set(div, 0) }
+        choosen = inventoryArr[4]
+        div.addEventListener("click", choose)
         inventory.append(div)
     }
 }
-function choose(){
-    if(this.hasChildNodes() &&this.children[0]!=choosen){
-        choosen.parentElement.style.background="transparent"
-        this.style.background="darkcyan"
-        choosen=this.children[0]
-        choosenClass=choosen.classList[0]}
+function choose() {
+    if (this.hasChildNodes() && this.children[0] != choosen) {
+        if (this === choosenElementOutLine) {
+            this.classList.toggle("outline");
+            choosenElementOutLine = null;
+            choosenElementToApply = null;
+            return
+        }
+        else if (inventoryArr.includes(this)) {
+            if (choosen) {
+                choosen.parentElement.style.background = "transparent"
+            }
+            if (choosenElementOutLine)
+                choosenElementOutLine.classList.remove("outline")
+            this.style.background = "darkcyan"
+            choosen = this.children[0]
+            choosenClass = choosen.classList[0]
+            choosenElementOutLine=null;
+            choosenElementToApply = null;
+        }
+        else {
+            if (choosen) {
+                choosen.parentElement.style.background = "transparent"
+            }
+            if (choosenElementOutLine)
+                choosenElementOutLine.classList.remove("outline")
+            choosenElementOutLine = this;
+            this.classList.add("outline")
+            choosen = null; choosenClass = null;
+        }
+    }
 }
 function createBoard() {
     for (let i = 0; i < gameSize[1]; i++) {//add sky
@@ -74,6 +99,7 @@ function createBoard() {
             const sky = document.createElement("div")
             board.append(sky)
             sky.classList.add("sky")
+            sky.addEventListener("click", apply)
             temp[j] = sky
         }
         mainArr.unshift(temp)
@@ -87,161 +113,224 @@ function createObjects() {
         mainArr[i].map(val => {
             val.setAttribute("class", "dirt")
             dirt.push(val)
-            val.addEventListener("click",removeDirt)
-            val.addEventListener("mouseover",highlightDirt)
-            val.addEventListener("mouseout",removeHighlight)
+            val.addEventListener("click", removeDirt)
+            val.addEventListener("mouseover", highlightDirt)
+            val.addEventListener("mouseout", removeHighlight)
         })
     }
     mainArr[dirtRows].map(val => {
         val.setAttribute("class", "grass")
         grass.push(val)
-        val.addEventListener("click",removeDirt)
-        val.addEventListener("mouseover",highlightDirt)
-        val.addEventListener("mouseout",removeHighlight)
+        val.addEventListener("click", removeDirt)
+        val.addEventListener("mouseover", highlightDirt)
+        val.addEventListener("mouseout", removeHighlight)
     })
-    tree=generateTree(dirtRows)
-    createTree(dirtRows,tree);//i send dirt rows because the main arr starts from 0 so no need to add 1
+    tree = generateTree(dirtRows)
+    createTree(dirtRows, tree);//i send dirt rows because the main arr starts from 0 so no need to add 1
     // createStones(dirtRows);
 }
 
 function generateTree(dirtRows) {//tree templates
     let tree1 = [["leaves", "leaves", "leaves", "leaves"],
-                ["leaves", "leaves", "leaves", "leaves"],
-                ["leaves", "log", "leaves", "leaves"],
-                ["sky", "log", "log", "sky"],
-                ["sky", "log", "log", "sky"],
-                ["sky", "log", "log", "sky"]]
+    ["leaves", "leaves", "leaves", "leaves"],
+    ["leaves", "log", "leaves", "leaves"],
+    ["sky", "log", "log", "sky"],
+    ["sky", "log", "log", "sky"],
+    ["sky", "log", "log", "sky"]]
     let tree2 = [["leaves", "leaves", "leaves"],
-                ["leaves", "leaves", "leaves"],
-                ["leaves", "log", "leaves"],
-                ["sky", "log", "sky"],
-                ["sky", "log", "sky"]]
+    ["leaves", "leaves", "leaves"],
+    ["leaves", "log", "leaves"],
+    ["sky", "log", "sky"],
+    ["sky", "log", "sky"]]
     let tree3 = [["leaves", "leaves", "leaves"],
-                ["leaves", "leaves", "leaves"],
-                ["leaves", "leaves", "leaves"],
-                ["sky", "log", "sky"],
-                ["sky", "log", "sky"],
-                ["sky", "log", "sky"]]
-    let tree4=[["sky", "leaves", "sky"],
-                ["leaves", "leaves", "leaves"],
-                ["leaves", "leaves", "leaves"],
-                ["sky", "log", "sky"],
-                ["sky", "log", "sky"],
-                ["sky", "log", "sky"]]
+    ["leaves", "leaves", "leaves"],
+    ["leaves", "leaves", "leaves"],
+    ["sky", "log", "sky"],
+    ["sky", "log", "sky"],
+    ["sky", "log", "sky"]]
+    let tree4 = [["sky", "leaves", "sky"],
+    ["leaves", "leaves", "leaves"],
+    ["leaves", "leaves", "leaves"],
+    ["sky", "log", "sky"],
+    ["sky", "log", "sky"],
+    ["sky", "log", "sky"]]
     let tree5;
-    if(boardSize===100){
-        tree5= [["leaves","leaves", "leaves", "leaves", "leaves","leaves"],
-        ["leaves","leaves", "leaves", "leaves", "leaves","leaves"],
-        ["leaves","leaves", "leaves", "leaves", "leaves","leaves"],
-        ["leaves","leaves", "log", "leaves", "leaves","leaves"],
-        ["leaves","leaves", "log", "log", "leaves","leaves"],
-        ["sky","sky", "log", "log", "sky", "sky"],
-        ["sky","sky", "log", "log", "sky", "sky"],
-        ["sky","sky", "log", "log", "sky", "sky"],
-        ["sky","sky", "log", "log", "sky", "sky"]]
-        if(dirtRows+tree5.length>12){
+    if (boardSize === 100) {
+        tree5 = [["leaves", "leaves", "leaves", "leaves", "leaves", "leaves"],
+        ["leaves", "leaves", "leaves", "leaves", "leaves", "leaves"],
+        ["leaves", "leaves", "leaves", "leaves", "leaves", "leaves"],
+        ["leaves", "leaves", "log", "leaves", "leaves", "leaves"],
+        ["leaves", "leaves", "log", "log", "leaves", "leaves"],
+        ["sky", "sky", "log", "log", "sky", "sky"],
+        ["sky", "sky", "log", "log", "sky", "sky"],
+        ["sky", "sky", "log", "log", "sky", "sky"],
+        ["sky", "sky", "log", "log", "sky", "sky"]]
+        if (dirtRows + tree5.length > 12) {
             tree5.pop()
             tree5.pop()
         }
-        if(dirtRows+tree5.length===12)
+        if (dirtRows + tree5.length === 12)
             tree5.pop();
-        random=Math.floor(Math.random()*5+1);
+        random = Math.floor(Math.random() * 5 + 1);
     }
-    else{
-        random=Math.floor(Math.random()*4+1);
+    else {
+        random = Math.floor(Math.random() * 4 + 1);
     }
-    switch (random){
-        case 1:return tree1
-        case 2:return tree2
-        case 3:return tree3
-        case 4:return tree4
-        case 5:return tree5
+    switch (random) {
+        case 1: return tree1
+        case 2: return tree2
+        case 3: return tree3
+        case 4: return tree4
+        case 5: return tree5
     }
 }
-function createTree(floor,tree){//draw any tree from tree templates
+function createTree(floor, tree) {//draw any tree from tree templates
 
-    let row=Math.floor(Math.random()*mainArr[0].length);
-    if(row>mainArr[0].length-tree[0].length){
-        row=mainArr[0].length-tree[0].length
+    let row = Math.floor(Math.random() * mainArr[0].length);
+    if (row > mainArr[0].length - tree[0].length) {
+        row = mainArr[0].length - tree[0].length
     }
-    let height=tree.length;//height of the tree we want to start from this number above ground
-    let start=height+floor
-    let temp=row
-    for (let i=0;i<tree.length;i++){
-        for (let j=0;j<tree[i].length;j++){
-            mainArr[start][row].setAttribute("class",`${tree[i][j]}`)
-            if(![...mainArr[start][row].classList].includes("sky")){
-                mainArr[start][row].addEventListener("click",cutTree)
-                mainArr[start][row].addEventListener("mouseover",highlightTree)
-                mainArr[start][row].addEventListener("mouseout",removeHighlight)
+    let height = tree.length;//height of the tree we want to start from this number above ground
+    let start = height + floor
+    let temp = row
+    for (let i = 0; i < tree.length; i++) {
+        for (let j = 0; j < tree[i].length; j++) {
+            mainArr[start][row].setAttribute("class", `${tree[i][j]}`)
+            if (![...mainArr[start][row].classList].includes("sky")) {
+                mainArr[start][row].addEventListener("click", cutTree)
+                mainArr[start][row].addEventListener("mouseover", highlightTree)
+                mainArr[start][row].addEventListener("mouseout", removeHighlight)
             }
             row++;
         }
-        row=temp
+        row = temp
         start--;
     }
 }
-function highlightTree(){
-    if(choosenClass==="axe")
-        this.style.border="3px solid black"
+function highlightTree() {
+    if (choosenClass === "axe")
+        this.style.border = "3px solid black"
 }
-function highlightDirt(){
-    if(choosenClass==="shovel")
-        this.style.border="3px solid black"
+function highlightDirt() {
+    if (choosenClass === "shovel")
+        this.style.border = "3px solid black"
 }
-function removeHighlight(e=false){(this||e).style.border="none"}
-function cutTree(){
-    addToInv(this,"axe")
+function removeHighlight(e = false) { (this || e).style.border = "none" }
+function cutTree() {
+    addToInv(this, "axe")
 }
-function removeDirt(){
-    addToInv(this,"shovel")
+function removeDirt() {
+    addToInv(this, "shovel")
 }
-function addToInv(element,item){
-    if(choosenClass===item){
-        let check=objectInInventory(element.classList[0]);
-        if(check){
-            emptyInventory.set(check,emptyInventory.get(check)+1)
+function addToInv(element, item) {
+    if (choosenClass === item) {
+        flag = false;
+        let check = objectInInventory(element.classList[0]);
+        if (check) {
+            emptyInventory.set(check, emptyInventory.get(check) + 1)
+            check.children[0].children[0].innerText = `${emptyInventory.get(check)}`
+            flag = true
         }
-        else{
-            let emptyKey=findFirstEmptyInventory()
-            let div=document.createElement("div")
-            div.classList.add(`${element.classList[0]}`,"taken-inv-item")
+        else if (findFirstEmptyInventory()) {
+            let emptyKey = findFirstEmptyInventory()
+            let div = document.createElement("div")
+            div.classList.add(`${element.classList[0]}`, "taken-inv-item")
+            emptyInventory.set(emptyKey, 1)
             emptyKey.append(div);
+            div.innerHTML = `<p style="user-select: none;" class="number-of-elements">${emptyInventory.get(emptyKey)}</p>`
+            div.addEventListener("click", chooseElemnt)
+            flag = true
         }
-        element.style.border="none"
-        element.setAttribute("class","sky")
-        var new_element = element.cloneNode(true);
-        element.parentNode.replaceChild(new_element, element);
+        if (flag) {
+            element.style.border = "none"
+            element.setAttribute("class", "sky")
+            var new_element = element.cloneNode(true);
+            new_element.addEventListener("click", apply)
+            element.parentNode.replaceChild(new_element, element);
+        }
     }
 }
-function objectInInventory(classCheck){
-    for (let [key,value] of emptyInventory){
-        if(key.children[0])
-            if( [...key.children[0].classList].includes(classCheck)){
+function objectInInventory(classCheck) {
+    for (let [key, value] of emptyInventory) {
+        if (key.children[0])
+            if ([...key.children[0].classList].includes(classCheck)) {
                 return key
             }
     }
     return 0;
 
 }
-function findFirstEmptyInventory(){
-    let toReturn;
-    for (let [key,val] of emptyInventory)
-    {
-        if(key.children.length===0)
-            toReturn=key
+function findFirstEmptyInventory() {
+    let toReturn = false;
+    for (let [key, val] of emptyInventory) {
+        if (key.children.length === 0)
+            toReturn = key
     }
     return toReturn
 }
-let startGame; let loadGame; let creator; let secondaryChoice; let container; let small; let big; let board;let inventory;//elemtns alreday on the screen(query selector)
+function chooseElemnt() {
+    choosenElementToApply = this.parentElement
+}
+function apply() {
+    console.log(this);
+    if (choosenElementToApply) {
+        if ([...this.classList].includes("sky")) {
+            this.setAttribute("class", `${choosenElementToApply.children[0].classList[0]}`)
+        }
+        let newAmmout = emptyInventory.get(choosenElementToApply) - 1;
+        if (newAmmout === 0) {
+            choosenElementToApply.children[0].setAttribute("class", "sky taken-inv-item")
+            choosenElementToApply.children[0].remove();
+            // var new_element = element.cloneNode(true);
+            // element.parentNode.replaceChild(new_element, element);
+            emptyInventory.set(choosenElementToApply, 0)
+        }
+        else {
+            choosenElementToApply.children[0].children[0].innerText = `${newAmmout}`
+            emptyInventory.set(choosenElementToApply, newAmmout)
+        }
+        reAddEvent(this)
+        if (choosenElementOutLine)
+            choosenElementOutLine.classList.remove("outline")
+        choosenElementToApply = null
+    }
+    console.log(this);
+}
+function reAddEvent(val) {
+    switch (val.classList[0]) {
+        case "leaves":
+            val.addEventListener("click", cutTree)
+            val.addEventListener("mouseover", highlightTree)
+            val.addEventListener("mouseout", removeHighlight)
+            break;
+        case "log":
+            val.addEventListener("click", cutTree)
+            val.addEventListener("mouseover", highlightTree)
+            val.addEventListener("mouseout", removeHighlight)
+            break;
+        case "dirt":
+            val.addEventListener("click", removeDirt)
+            val.addEventListener("mouseover", highlightDirt)
+            val.addEventListener("mouseout", removeHighlight)
+            break;
+        case "grass":
+            val.addEventListener("click", removeDirt)
+            val.addEventListener("mouseover", highlightDirt)
+            val.addEventListener("mouseout", removeHighlight)
+            break;
+    }
+}
+let startGame; let loadGame; let creator; let secondaryChoice; let container; let small; let big; let board; let inventory;//elemtns alreday on the screen(query selector)
 let gameSize;//array of cols and rows
 let mainArr = [];//array displaying board
 let boardSize;//board size in vw
-let grass=[];//grass blocks
-let dirt=[];//dirt blocks
-let inventoryArr=[];let emptyInventory=new Map();
+let grass = [];//grass blocks
+let dirt = [];//dirt blocks
+let inventoryArr = []; let emptyInventory = new Map();
 let choosen;//choosen inventory item
-let choosenClass;
+let choosenElementToApply;//choosen element
+let choosenClass; let choosenElementOutLine
+const invSpace = 8;//inv size
 createElements();
 assignEvenListeners()
 
